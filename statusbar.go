c28da79e3da393beff571562ecef35a6dbc63dcb
Hilde N <snhilde@protonmail.com>
2019-11-03 14:32:27 -0500

    add comments

--- statusbar.go
+++ statusbar.go
@@ -1,17 +1,21 @@
+// Package statusbar displays various resources on the DWM statusbar.
 package statusbar
 
 import (
 	"fmt"
 )
 
+// Routine interface allows resource monitors to be linked in.
 type Routine interface {
 	Update() error
 	String() string
 	Sleep()
 }
 
+// Bar type is the main object for the package.
 type Bar []Routine
 
+// Creates a new Bar.
 func New() Bar {
 	var b Bar
 	return b
