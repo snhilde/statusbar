42d01eb08d9cf0b4430984f8fbdde4df56a53336
Hilde N <snhilde@protonmail.com>
2020-01-13 21:37:01 -0500

    add Append example

--- example_test.go
+++ example_test.go
@@ -1,1 +1,20 @@
 package statusbar_test
+
+import (
+	"github.com/snhilde/sb4routines/sbtime"
+)
+
+func ExampleStatusbar_Append() {
+	bar := statusbar.New()
+
+	time_fmt := "Jan 2 - 03:04"
+
+	normal_color := "#FFFFFF"
+	warning_color := "#BB4F2E"
+	error_color := "#A1273E"
+	colors := [3]string{normal_color, warning_color, error_color}
+
+	time_routine := sbtime.New(time_fmt, colors)
+
+	bar.Append(time_routine, 1)
+}
