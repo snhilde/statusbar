b547b470a9ef4cff39cae6a99389336001390dc8
Hilde N <snhilde@protonmail.com>
2020-01-13 21:41:08 -0500

    add comments to Append example

--- example_test.go
+++ example_test.go
@@ -5,8 +5,9 @@ import (
 )
 
 func ExampleStatusbar_Append() {
-	bar := statusbar.New()
+	// Add the sbtime routine to our statusbar.
 
+	// sbtime.New() takes two arguments: format to use for the time string and triplet of color codes.
 	time_fmt := "Jan 2 - 03:04"
 
 	normal_color := "#FFFFFF"
@@ -14,8 +15,10 @@ func ExampleStatusbar_Append() {
 	error_color := "#A1273E"
 	colors := [3]string{normal_color, warning_color, error_color}
 
+	// Create a new routine.
 	time_routine := sbtime.New(time_fmt, colors)
 
+	// Append the routine to the bar. Routines will be displayed in order of addition to the bar object.
 	bar.Append(time_routine, 1)
 
 	// Or, as a one-liner:
