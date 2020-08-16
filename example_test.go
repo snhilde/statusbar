add3605bd95f3e22ed58a6411dc36dcce538a22f
Hilde N <snhilde@protonmail.com>
2020-08-16 08:33:23 -0400

    fix variable name format

--- example_test.go
+++ example_test.go
@@ -8,18 +8,18 @@ func ExampleStatusbar_Append() {
 	// Add the sbtime routine to our statusbar.
 
 	// sbtime.New() takes two arguments: format to use for the time string and triplet of color codes.
-	time_fmt := "Jan 2 - 03:04"
+	timeFmt := "Jan 2 - 03:04"
 
-	normal_color := "#FFFFFF"
-	warning_color := "#BB4F2E"
-	error_color := "#A1273E"
-	colors := [3]string{normal_color, warning_color, error_color}
+	colorNormal := "#FFFFFF"
+	colorWarning := "#BB4F2E"
+	colorError := "#A1273E"
+	colors := [3]string{colorNormal, colorWarning, colorError}
 
 	// Create a new routine.
-	time_routine := sbtime.New(time_fmt, colors)
+	timeRoutine := sbtime.New(timeFmt, colors)
 
 	// Append the routine to the bar.
-	bar.Append(time_routine, 1)
+	bar.Append(timeRoutine, 1)
 
 	// Or, as a one-liner:
 	bar.Append(sbtime.New("Jan 2 - 03:04", [3]string{"#FFFFFF", "#BB4F2E", "#A1273E"}), 1)
