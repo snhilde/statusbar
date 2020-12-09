6e741a865bda9758b2ef9d322aa091fd1b1b2950
Hilde N <snhilde@protonmail.com>
2020-12-08 20:33:10 -0500

    set up handlers for GET, PUT, and DELETE methods

--- api.go
+++ api.go
@@ -1,10 +1,14 @@
 package statusbar
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
 	"net/http"
-	"log"
 )
+
+type failure struct {
+	Message string `json:"error"`
+}
 
 // listen starts the REST API microservice and handles requests.
 func listen(rs []routine) {
@@ -14,10 +18,36 @@ func listen(rs []routine) {
 		routines[v.pkgName] = v
 	}
 
-	http.HandleFunc("/api/v1", test)
-	log.Fatal(http.ListenAndServe(":6777", nil))
+	// Add a handler for the REST API.
+	http.HandleFunc("/rest/v1", handleRest)
+
+	// Start listening on our REST port.
+	err := http.ListenAndServe(":3777", nil)
+	if err == nil {
+		err = errors.New("REST API down")
+	}
+	logError(err.Error())
 }
 
-func test(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "It works")
+func handleRest(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handleGet(w, r)
+	case "PUT":
+		handlePut(w, r)
+	case "DELETE":
+		handleDelete(w, r)
+	default:
+		e := json.NewEncoder(w)
+		e.Encode(failure{"Invalid method"})
+	}
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+}
+
+func handlePut(w http.ResponseWriter, r *http.Request) {
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) {
 }
