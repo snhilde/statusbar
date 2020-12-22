422f0c788764ccd9ac148356131167a79bd8c3af
Hilde N <snhilde@protonmail.com>
2020-12-22 17:55:47 -0500

    fix format

--- restapi/restapi.go
+++ restapi/restapi.go
@@ -219,7 +219,7 @@ func (e *Engine) Stop() error {
 	}
 
 	// Give the engine 5 seconds to close out any connections.
-	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	err := e.server.Shutdown(ctx)
