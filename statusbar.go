371a7eccc49657236b07ad8694d287eccf543226
Hilde N <snhilde@protonmail.com>
2020-12-31 13:24:21 -0500

    handle SIGTERM signal

--- statusbar.go
+++ statusbar.go
@@ -14,6 +14,7 @@ import (
 	"os/signal"
 	"reflect"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -274,7 +275,7 @@ func setBar(s string) {
 // handleSignal clears the statusbar if the program receives an interrupt signal.
 func (sb *Statusbar) handleSignal() {
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 
 	// Wait until we receive an interrupt signal.
 	<-c
